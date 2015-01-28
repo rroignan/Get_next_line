4c7a5f2bffc350c13608b45053929c45eaa4f811
Robin ROIGNANT <rroignan@e1r4p20.42.fr>
2015-01-28 20:48:00 +0100

    updated

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,19 @@
 /*   By: rroignan <rroignan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/01/20 15:00:52 by rroignan          #+#    #+#             */
-/*   Updated: 2015/01/28 19:23:13 by rroignan         ###   ########.fr       */
+/*   Updated: 2015/01/28 20:47:34 by rroignan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+static void		ft_fill(char ***str, char **stk, char **tmp, char **fub)
+{
+	**str = ft_strsub(*tmp, 0, (ft_strchr(*tmp, '\n') - *tmp));
+	*stk = ft_strsub(*tmp, (ft_strchr(*tmp, '\n') - *tmp) + 1, ft_strchr(*tmp, '\0') - ft_strchr(*tmp, '\n'));
+	ft_strdel(tmp);
+	ft_strdel(fub);
+}
 
 int		get_next_line(int fd, char **line)
 {
@@ -32,10 +40,7 @@ int		get_next_line(int fd, char **line)
 	}
 	if (temp != NULL && ft_strchr(temp, '\n') != NULL)
 	{
-		*line = ft_strsub(temp, 0, (ft_strchr(temp, '\n') - temp));
-		stockage = ft_strsub(temp, (ft_strchr(temp, '\n') - temp) + 1, ft_strchr(temp, '\0') - ft_strchr(temp, '\n'));
-		ft_strdel(&temp);
-		ft_strdel(&buf);
+		ft_fill(&line, &stockage, &temp, &buf);
 		return (1);
 	}
 	while ((ret = read(fd, buf, BUF_SIZE)) > 0)
@@ -56,9 +61,6 @@ int		get_next_line(int fd, char **line)
 		ft_strdel(&stockage);
 		return (1);
 	}
-	*line = ft_strsub(temp, 0, (ft_strchr(temp, '\n') - temp));
-	stockage = ft_strsub(temp, (ft_strchr(temp, '\n') - temp) + 1, ft_strchr(temp, '\0') - ft_strchr(temp, '\n'));
-	ft_strdel(&temp);
-	ft_strdel(&buf);
+	ft_fill(&line, &stockage, &temp, &buf);
 	return (1);
 }

b5f277ca5350d08055ccccf96f93ba6e8daf2a47
Robin ROIGNANT <rroignan@e1r4p22.42.fr>
2015-02-02 12:20:33 +0100

    .h norme

--- get_next_line.h
+++ get_next_line.h
@@ -6,20 +6,20 @@
 /*   By: rroignan <rroignan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/12/03 10:58:33 by rroignan          #+#    #+#             */
-/*   Updated: 2015/01/28 19:19:38 by rroignan         ###   ########.fr       */
+/*   Updated: 2015/02/02 11:28:38 by rroignan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# include "libft.h"
+# include "./libft/libft.h"
 # include <fcntl.h>
 # include <unistd.h>
 # include <stdlib.h>
 
-# define BUF_SIZE 7
+# define BUF_SIZE 8
 
-int		get_next_line(int const fd, char ** line);
+int		get_next_line(int const fd, char **line);
 
 #endif
